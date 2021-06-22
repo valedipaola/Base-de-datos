a177b0fcb28c65793d491822a06c78be6d4b6db6
valedipaola <80895508+valedipaola@users.noreply.github.com>
2021-06-22 20:30:32 -0300

    Add files via upload

---
+++ MySQL2.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE EXAMENNN;
+USE EXAMENNN;
+CREATE TABLE ESTUDIANTES ( 
+idest int  not null primary key,
+
+nombre varchar(12) not null,
+
+apellido varchar(12) not null,
+
+nota int not null,
+
+curso varchar(40) not null);
+
